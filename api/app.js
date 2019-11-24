c6e23b6919a7abcfad51c1d7d349ff4dda9ba637
bmortella <bmortella@gmail.com>
2019-11-24 06:10:06 -0300

    Registro funcional com API

--- api/app.js
+++ api/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 var app = express();
 
+app.use(express.static('../web'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 let db = new sqlite3.Database('./database.db', (err) => {
   if (err) {
     console.error(err.message);
@@ -9,15 +13,14 @@ let db = new sqlite3.Database('./database.db', (err) => {
   console.log('Connected to the database.');
 });
 
-
 app.post('/api/users/register', function(req, res) {
-  db.run(`
-    INSERT INTO users VALUES (?, ?, ?, ?, ?)
-  `, [req.query.email, req.query.senha, req.query.nome, req.query.sobrenome, req.query.cnh])
+   db.run(`
+     INSERT INTO users(Email, Senha, Nome, Sobrenome, CPF, CNH) VALUES (?, ?, ?, ?, ?, ?)
+   `, [req.body.args.email, req.body.args.password, req.body.args.name, req.body.args.lastname, req.body.args.cpf, ""])
   res.sendStatus(200)
 });
 
-app.use(express.static('../web'))
+
 app.listen(3000, function () {
     db.run(`CREATE TABLE IF NOT EXISTS 
     users(
@@ -26,9 +29,11 @@ app.listen(3000, function () {
       Senha VARCHAR(20),
       Nome VARCHAR(15),
       Sobrenome VARCHAR(15),
+      CPF VARCHAR(11),
       CNH VARCHAR(10),
       UNIQUE (Email),
-      UNIQUE (CNH)
+      UNIQUE (CNH),
+      UNIQUE (CPF)
     )`)
-    console.log('Example app listening on port 3000!');
+    console.log('Ready :3000!');
 });
